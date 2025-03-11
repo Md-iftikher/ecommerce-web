0ce943fd48f6923e3b021789d568c5941f36cf22
Nazeef Tawfeeq <nazeef.tawfeeq@gmail.com>
2025-03-11 20:41:32 +0600

    fixed paths

--- Client/php/my_profile/update_address.php
+++ Client/php/my_profile/update_address.php
@@ -1,5 +1,5 @@
 <?php 
-include "config.php";
+include "../config.php";
 $id = (int) $_POST['id'];
 $old_address = $_POST['old_address'];
 $address = $_POST['address'];
@@ -12,7 +12,7 @@ where cust_id = $id and address = '$old_address';
 ";
 
 if($conn->query($sql)) {
-    header("location: ../my_profile.php?address=true&success=true");
+    header("location: ../../my_profile.php?address=true&success=true");
 }
 
 ?>
