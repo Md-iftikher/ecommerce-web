39e5ff8b9803eeb548c9843bfe08132cb9ac5fbe
Nazeef Tawfeeq <nazeef.tawfeeq@gmail.com>
2025-04-16 23:46:37 +0600

    added search bar to all products

---
+++ Client/php/products/search_products.php
@@ -0,0 +1,30 @@
+<?php
+include_once __DIR__ . "/../config.php";
+
+$input = json_decode(file_get_contents("php://input"), true);
+$pattern = $input['pattern'] ?? '';
+
+$sql = "
+SELECT p.*, c.category_name
+FROM products p
+INNER JOIN categories c ON p.category_id = c.category_id
+WHERE p.product_name LIKE ?;
+";
+
+$stmt = $conn->prepare($sql);
+$likePattern = "%" . $pattern . "%";
+$stmt->bind_param("s", $likePattern);
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+$products = [];
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $products[] = $row;
+    }
+}
+
+echo json_encode($products);
+?>
